31141b75d90df889080c40d2f239e5918fe695c7
Charles-Antoine LEGER <ca@leger.email>
2020-05-18 15:55:39 +0200

    Add styled components

---
+++ src/themes/theme.js
@@ -0,0 +1,26 @@
+export default {
+  fonts: {
+    main: "Muli, sans-serif",
+    code: "Roboto Mono, monospace",
+  },
+  colors: {
+    main1: "hsl(207, 70%, 59%)",
+    main1: "hsl(207, 70%, 94%)",
+  },
+  breakpoints: {
+    mobile: "only screen and (max-width: 50rem)",
+    talet: "only screen and (max-width: 65rem)",
+  },
+  spacings: {
+    small: "1rem",
+    medium: "2rem",
+    large: "3rem",
+  },
+  animations: {
+    button: "box-shadow 0.3s ease",
+    link: "color 0.2s ease",
+  },
+  shadows: {
+    shadow1: "0px 5px 20px rgba(30, 30, 31, 0.05)",
+  },
+}
